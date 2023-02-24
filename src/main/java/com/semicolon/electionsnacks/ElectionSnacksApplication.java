ac77a8bc1c64869ae2e2abf07cb661de2173ad11
Hadiza <deezah30@gmail.com>
2023-02-24 14:10:21 +0100

    first-commit

---
+++ src/main/java/com/semicolon/electionsnacks/ElectionSnacksApplication.java
@@ -0,0 +1,13 @@
+package com.semicolon.electionsnacks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectionSnacksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElectionSnacksApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/semicolon/electionsnacks/ElectionSnacksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.semicolon.electionsnacks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectionSnacksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
