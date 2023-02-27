b69f79104d9454bac878d66d79023195ad731ceb
Hadiza <deezah30@gmail.com>
2023-02-27 18:27:24 +0100

    entities

---
+++ src/main/java/models/Voter.java
@@ -0,0 +1,27 @@
+package models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Getter
+@Setter
+@Entity
+public class Voter {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String lastName;
+    private String firstName;
+    private String Gender;
+    private String dateOfBirth;
+    private String maritalStatus;
+    private String LgaOfOrigin;
+    private String state;
+    private String residentialAddress;
+    private Boolean isVerified;
+}
